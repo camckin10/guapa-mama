c95d18e86db2bacb52339ad331a28a91c827db2b
Chelsey McKinney <chelseymckinney@gmail.com>
2019-08-05 08:33:40 -0500

    pushing latest code

--- fayre_jean/src/Components/Bio.js
+++ fayre_jean/src/Components/Bio.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.section`
+	background:thistle;
+	padding:4em;	
+`;
 
+const Biography = styled.h3`
+	font-size:1.5em;
+	color:purple;
+	
+`;
+
+const Paragraph = styled.p`
+	color:black;
+	font-weight:300;
+	font-style:bold;
+	font-family:sans-serif;
+	
+`;
 
 class Bio extends Component {
 	render() {
 		return (
 			<div className="bio-section">
-				<h3>About Me</h3>
-				<p>Quit running around this house like you ain't got no goddamn sense! God ain't through with me yet! Don't ask me for nothing else. I don't need to explain myself! I'm grown! Do you pay my automo-bills? Roll your eyes again, imma slap em to the back of yo head.</p>
+				<Wrapper>
+					<Biography>About Me</Biography>
+					<Paragraph>
+					Quit running around this house like you ain't got no goddamn sense! God ain't through with me yet! Don't ask me for nothing else. I don't need to explain myself! I'm grown! Do you pay my automo-bills? Roll your eyes again, imma slap em to the back of yo head.
+					</Paragraph>
+				</Wrapper>
+				
 			</div>
 		);
 	}
--- fayre_jean/src/Components/Header.js
+++ fayre_jean/src/Components/Header.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+		background: lavender;
+		padding:4em;
+`;
+
+const Title = styled.h1`
+		color:purple;
+		font-size:2.5em;
+		font-family:sans-serif
+`;
+
+const Subtitle = styled.h2`
+		color:black;
+		font-style:bold;
+		font-weight:300;
+		font-family:sans-serif;
+`;
 
 class Header extends Component {
 	render () {
 		return (
 			<div className="header-section">
-				<h1>GUAPA MAMA</h1>
-				<h2>Fundraiser | Teacher | Grant writer</h2>
+				<Wrapper>
+					<Title>Fayre Crossley</Title>
+					<Subtitle>Fundraiser | Teacher | Grant writer</Subtitle>
+				</Wrapper>
+				
 			</div>
 		);
 	}
